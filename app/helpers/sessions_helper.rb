4a3ee94599b8d2cdb4cf1a102fd05a7df9b0bbfb
V <v_534@hotmail.com>
2012-08-28 12:54:40 -0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,8 +17,21 @@ module SessionsHelper
     # "||=" only assigns if variable nil
   end    
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
